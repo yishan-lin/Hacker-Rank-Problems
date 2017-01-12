6bb9501d372bbe8beea2b2ff0da09f050929c92e
ylin10 <ylin10@bu.edu>
2017-01-11 20:09:54 -0800

    Setup

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -2,34 +2,33 @@ public class LinkedListPractice {
     
     class Node {
      
-        public int item;
-        public Node next;
+        int item;
+        Node next;
         
         Node() {
-            
+         
             item = 0;
             next = null;
             
         }
         
-        Node(int x) {
+        Node (int x) {
             
             item = x;
             next = null;
             
         }
         
-        Node(int x, Node y) {
-            
+        Node (int x, Node y) {
+         
             item = x;
             next = y;
             
         }
+        
     };
     
     public static void main(String[] args) {
-        
-        
         
     }
     
