4b4de9cfe6eaa207eca760be2b7231074aef6bdc
ylin10 <ylin10@bu.edu>
2017-01-12 11:20:56 -0800

    Practicing a delete() method
    Delete method

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -44,12 +44,44 @@ public class LinkedListPractice {
         }
     }
     
+    static Node deleteNode(Node head, int item) {
+     
+        if (head == null) {
+        
+            System.out.println("Didn't find " + item + " in LL.");
+            return null;
+        }
+        
+        else if (head.item == item) {
+            
+            System.err.println("Found " + item + "in LL.");
+            System.err.println("head.item = " + head.item);
+            System.err.println("head.next.item = " + head.next.item);
+            System.err.println("head.next.next.item = " + head.next.next.item);
+            head.next = head.next.next;
+
+        }
+        
+        else {
+            
+            head.next = deleteNode(head.next, item);
+        }
+        
+        return head;
+        
+    }
+    
     public static void main(String[] args) {
         
         Node head = new Node (3, new Node(6, new Node(9, new Node (12))));
         
         printLL(head);
         
+        System.out.println();
+        
+        deleteNode(head, 5);
+        
+        printLL(deleteNode(head, 9));
     }
     
 }
