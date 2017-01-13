b5ecf199720f537f85fdf2dabf2c7343bbe89122
ylin10 <ylin10@bu.edu>
2017-01-12 18:27:59 -0800

    Final Solution
    Learned how/when to use Hashset.  Getting a bit better at Linked Lists.
     Previous solution still works, but it’s pretty convoluted method.
    
    The previous method was:
    1) read into Hashtable, assign a value of # of instances it repeated.
    2) take out only the keys that had values where the # of instances was
    > 1
    3) Return those keys as an arrayList
    4) Run those arrayList against the original linked list, then for every
    one time it spots one of those integers, it skips to the next one
    5) However, if the next one it’s supposed to skill to is also a
    duplicate, then it goes to the one after.
    
    Super convoluted. Took a long time.  Good enough to be a p-set.

--- RemoveDuplicatesLLwithHashSet.java
+++ RemoveDuplicatesLLwithHashSet.java
@@ -29,16 +29,70 @@ public class RemoveDuplicatesLLwithHashSet {
         }
     };
     
-    void deleteduplicates(Node head) {
+    static Node removeduplicates(Node head) {
+     
+        HashSet<Integer> linkedlist = new HashSet <Integer>();
+        Node previous = null;
+        Node current = head;
         
+        while (current != null) {
+            
+            if (linkedlist.contains(current.item)) {
+             
+                previous.next = current.next;
+                
+            }
+            
+            else {
+             
+                linkedlist.add(current.item);
+                
+                previous = current;
+                
+            }
+            
+            current = current.next;
+            
+        }
+        
+        return head;
+    }
+    
+    static void printLL(Node head) {
+     
+        if (head == null) {
+            
+            System.out.println("can't print out empty arrays.");
+            
+        }
+        
+        while (head != null) {
+            
+            if (head.next == null) {
+                
+                System.out.print(head.item + ".");
+                break;
+            
+            }
+            
+            System.out.print(head.item + " => ");
+            head = head.next;
+        
+        }
         
     }
     
     public static void main(String[] args) {
         
-      Node head = new Node(3, new Node(6, new Node(1, new Node(8, new Node(3, new Node(7, new Node(10)))))));  
-      
-      
+        Node head = new Node(3, new Node(6, new Node(1, new Node(8, new Node(3, new Node (6, new Node(7, new Node(10)))))))); 
+        System.err.println("Original head:");
+        printLL(head);
+        System.err.println("After removing duplicates:");
+        printLL(removeduplicates(head));
+        
+//printLL(head);
+        //printLL(deleteduplicates(head));
+        
         
     }
     
