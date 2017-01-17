e23cd0e14ce03ca80efa0dca69dec6661341aa2d
ylin10 <ylin10@bu.edu>
2017-01-17 15:10:54 -0800

    Practice

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -5,21 +5,21 @@ public class LinkedListPractice {
         public int item;
         public Node next;
         
-        Node() {
+        public Node() {
             
             item = 0;
             next = null;
             
         }
         
-        Node(int x) {
+        public Node(int x) {
             
             item = x;
             next = null;
             
         }
         
-        Node(int x, Node y) {
+        public Node(int x, Node y) {
             
             item = x;
             next = y;
@@ -28,142 +28,7 @@ public class LinkedListPractice {
         
     };
     
-    static void printLL(Node head) {
-        
-        if (head == null) {
-            
-            System.out.println("null");
-            
-        }
-        
-        else if (head.next == null) {
-            
-            System.out.print(head.item + ".");
-            
-        }
-        
-        else {
-            
-            System.out.print(head.item + " => ");
-            printLL(head.next);
-            
-        }
-    }
-    
-    static Node addtofront(Node head, int target) {
-        
-        head = new Node(target, head);
-        
-        return head;
-        
-    }
-    
-    static Node addtorear(Node head, int target) {
-        
-        if (head == null) {
-            
-            return new Node(target);
-            
-        }
-        
-        if (head.next == null) {
-            
-            head.next = new Node (target, null);
-            
-        }
-        
-        else {
-            
-            head.next = addtorear(head.next, target);
-            
-        }
-        
-        return head;
-        
-    }
-    
-    static Node insertinOrder(Node head, int target) {
-        
-        if (head == null) {
-            
-            return new Node(target);
-            
-        }
-        
-        else if (head.item > target) {
-            
-            return new Node(target, head);
-            
-        }
-        
-        else {
-            
-            head.next = insertinOrder(head.next, target);
-            
-        }
-        
-        return head;
-        
-    }
-    
-    static Node delete(Node head, int target) {
-        
-        Node headcopy = head;
-     
-        while (headcopy.next != null) {
-            
-            if (headcopy.next.item == target) {
-            
-                System.err.println("found!");
-                headcopy.next = headcopy.next.next;
-                
-            }
-         
-            headcopy = headcopy.next;
-            
-        }
-            
-       return head;
-    }
-    
     public static void main(String[] args) {
         
-        Node head = new Node (3, new Node(6, new Node(9, new Node (12))));
-        
-        printLL(null);
-        
-        printLL(delete(head, 6));
-        
-        /*printLL(head);
-        System.out.println();
-        printLL(emptyLL);
-        
-        printLL(addtofront(head, 10));
-        
-        System.out.println();
-        
-        printLL(addtorear(head, 14));
-        
-        System.out.println();
-        
-        printLL(insertinOrder(head, 10));
-        
-        System.out.println();
-        
-        printLL(insertinOrder(head, 2));
-        
-        System.out.println();
-        
-        printLL(insertinOrder(head, 7));
-        
-        System.out.println();
-        
-        System.err.println("testing delete");
-        
-        printLL(delete(head, 6));
-        
-        System.out.println();*/
-        
     }
-    
 }
