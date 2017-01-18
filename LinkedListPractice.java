d482059805f9b6e5a14ef67d6e5431089455103b
ylin10 <ylin10@bu.edu>
2017-01-17 16:16:53 -0800

    Practice

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -28,9 +28,29 @@ public class LinkedListPractice {
         
     };
     
+    static void printLL(Node head) {
+
+        if (head == null) {
+            System.out.println("empty array.");
+        }
+        
+        else if (head.next == null) {
+            System.out.print(head.item + ".");            
+        }
+        
+        else {
+            
+            System.out.print(head.item + " =>");
+            printLL(head.next);
+        }
+        
+    }
+    
     public static void main(String[] args) {
         
         Node head = new Node(3, new Node(6, new Node(9, new Node(12))));
+        
+        printLL(head);
         
     }
 }
