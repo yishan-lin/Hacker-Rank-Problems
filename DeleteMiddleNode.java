60d0ae5437b5610cb8e2cc403832ae0bd4fd6e98
ylin10 <ylin10@bu.edu>
2017-01-17 19:12:03 -0800

    Working through problem

--- DeleteMiddleNode.java
+++ DeleteMiddleNode.java
@@ -27,9 +27,49 @@ public class DeleteMiddleNode {
         }
         
     };
+    
+    private static int count = 0;
+    
+    static void deletemiddleNode(Node head) {
+     
+        Node headcopy = head;
+        
+        while (headcopy != null) {
+            
+            count += 1;
+            System.out.println(head.item + " @ " + count);
+            headcopy = headcopy.next;
+            
+        }
+        
+        System.out.println("the length of the head = " + count);
+        
+        int halfway = count/2;
+        
+        int second_count = 0;
+        
+        Node previous = head;
+        
+        while (head != null) {
+            
+            second_count += 1;
+            
+            if (second_count == halfway) {
+                
+                previous.next = head.next;
+            }
+            
+            previous = head;
+            head = head.next;
+            
+        }
+        
+    }
       
     public static void main(String[] args) {
         
+        Node head = new Node(3, new Node(6, new Node(9, new Node(12))));
+        deletemiddleNode(head);
     }
     
 }
