c7635d63e1679f7d875c4650b01c9926ee6af228
ylin10 <ylin10@bu.edu>
2017-01-17 18:04:12 -0800

    Final Solution
    wat

--- kthtolast.java
+++ kthtolast.java
@@ -1,34 +1,71 @@
 public class kthtolast {
-
+    
     static class Node {
         
         public int item;
         public Node next;
         
-    public Node() {
+        public Node() {
+            
+            item = 0;
+            next = null;
+            
+        }
         
-        item = 0;
-        next = null;
-    
-    }
-    
-    public Node(int x) {
+        public Node(int x) {
+            
+            item = x;
+            next = null;
+            
+        }
         
-        item = x;
-        next = null;
+        public Node(int x, Node y) {
+            
+            item = x;
+            next = y;
+            
+        }
         
-    }
-    
-    public Node(int x, Node y) {
-        
-        item = x;
-        next = y;
-        
-    }
-    
     };
     
+    private static int count = 0;
+    
+    static void kthtolast(Node head, int k) {
+        
+        while (head != null) {
+            
+            count += 1;
+            
+            if (count == k) {
+                
+                while (head != null) {
+             
+                System.out.println(head.item);
+                
+                head = head.next;
+                
+                }
+                
+            }
+            
+            else {
+            
+            //System.out.println("head.item = " + head.item + " @ " + count);
+            
+            head = head.next;
+            
+            }
+            
+        }
+        
+        
+    }
+    
     public static void main(String[] args) {
+        
+        Node head = new Node(3, new Node(6, new Node(9, new Node(12))));
+        
+        kthtolast(head, 2);
         
     }
     
