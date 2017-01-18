ffc30139898f1a3f346eaf7cc1ad8b51b43ae96c
ylin10 <ylin10@bu.edu>
2017-01-17 16:32:18 -0800

    Insert/Remove methods

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -29,28 +29,83 @@ public class LinkedListPractice {
     };
     
     static void printLL(Node head) {
-
+     
         if (head == null) {
-            System.out.println("empty array.");
+            System.err.println("empty array.");
+        }
+        else if (head.next == null) {
+            System.out.print(head.item + ".");
+        }
+        else {
+            System.out.print(head.item + " => ");
+            printLL(head.next);
         }
         
-        else if (head.next == null) {
-            System.out.print(head.item + ".");            
+    }
+    
+    static Node removeNode(Node head, int target) {
+     
+        Node headcopy = head;
+        
+        if (head == null) {
+            System.err.println(target + " was not found.");
+        }
+        else if (headcopy.next.item == target) {
+            headcopy.next = headcopy.next.next;
+        }
+        else {
+         
+            headcopy.next = removeNode(headcopy.next, target);
+            
+        }
+        
+        return head;
+        
+    }
+    
+    static Node addtofront(Node head, int target) {
+     
+        head = new Node(target, head);
+        
+        return head;
+        
+    }
+    
+    static Node insertinorder(Node head, int target) {
+        
+        Node headcopy = head;
+        
+        if (head == null) {
+            
+            head = new Node(target, head);
+            
+        }
+        
+        else if (headcopy.next.item > target) {
+            
+            headcopy.next = new Node(target, headcopy.next);
+            
         }
         
         else {
             
-            System.out.print(head.item + " =>");
-            printLL(head.next);
+            headcopy.next = insertinorder(headcopy.next, target);
+            
         }
+     
+        return head;
         
     }
     
     public static void main(String[] args) {
         
         Node head = new Node(3, new Node(6, new Node(9, new Node(12))));
-        
+
         printLL(head);
+        System.out.println();
+        printLL(removeNode(head, 6));
+        System.out.println();
+        printLL(insertinorder(head, 6));
         
     }
 }
