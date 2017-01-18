cb5731690f8e546c11722467b29fa21d1a0cb167
ylin10 <ylin10@bu.edu>
2017-01-17 17:47:58 -0800

    Kth to Last
    Write an algorithm for a singly linked list that prints and returns the
    elements from the linked list onwards.

---
+++ kthtolast.java
@@ -0,0 +1,35 @@
+public class kthtolast {
+
+    static class Node {
+        
+        public int item;
+        public Node next;
+        
+    public Node() {
+        
+        item = 0;
+        next = null;
+    
+    }
+    
+    public Node(int x) {
+        
+        item = x;
+        next = null;
+        
+    }
+    
+    public Node(int x, Node y) {
+        
+        item = x;
+        next = y;
+        
+    }
+    
+    };
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
